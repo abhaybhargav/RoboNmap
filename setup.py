7468e9c2df102364d0a37d7c7e95db64c0df3410
Abhay Bhargav <abhay@we45.com>
2018-04-28 14:31:17 +0530

    Minor changes to setup. Added to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 install_dependencies = (
     'python-libnmap==0.7.0',
@@ -7,16 +15,17 @@ install_dependencies = (
 
 setup(
     name='RoboNmap',
-    version='0.1',
+    version='1.1',
     packages=[''],
     package_dir={'': 'robonmap'},
     url='',
     license='MIT License',
     author='Abhay Bhargav',
-    author_email='Twitter: @abhaybhargav',
     install_requires = [
     'python-libnmap==0.7.0',
     'robotframework==3.0.4'
     ],
-    description=''
+    description='RoboNmap - Robot Framework Library for the Nmap Port and Vulnerability Scanner',
+    long_description = long_description,
+    long_description_content_type='text/markdown'
 )
